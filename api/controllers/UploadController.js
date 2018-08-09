f5bde170522777e9723cc601305f44bd2c983833
vsadh-jft <vsadh@jellyfishtechnologies.com>
2018-08-09 13:28:13 +0530

    uuid, listener in bootstrap

--- api/controllers/UploadController.js
+++ api/controllers/UploadController.js
@@ -4,6 +4,7 @@
  * @description :: Server-side logic for managing uploads
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
+var uuid = require('uuid');
 var Web3 = require('web3');
 var web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:8545'));
 const address = '0x14e6b19304fb3c98a73acea5f80d2c056ee5c09f'; // user
@@ -20,15 +21,17 @@ module.exports = {
 
     web3.eth.defaultAccount = web3.eth.accounts[1];
 
-    var contractInfo = JSON.parse('{"contract_name": "FileDetail","abi":[{"constant":false,"inputs":[{"name":"fName","type":"string"},{"name":"fileHash","type":"string"}],"name":"setFile","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"anonymous":false,"inputs":[{"indexed":false,"name":"fileName","type":"string"},{"indexed":false,"name":"fileHash","type":"string"}],"name":"Uploaded","type":"event"},{"constant":true,"inputs":[{"name":"index","type":"uint256"}],"name":"getFile","outputs":[{"name":"fileName","type":"string"},{"name":"fileHash","type":"string"}],"payable":false,"stateMutability":"view","type":"function"}]  }');
+    var contractInfo = JSON.parse('{"contract_name": "FileDetail","abi":[{"constant":false,"inputs":[{"name":"uuid","type":"string"},{"name":"fName","type":"string"},{"name":"fileHash","type":"string"}],"name":"setFile","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"anonymous":false,"inputs":[{"indexed":false,"name":"fileName","type":"string"},{"indexed":false,"name":"fileHash","type":"string"}],"name":"Uploaded","type":"event"},{"constant":true,"inputs":[{"name":"uuid","type":"string"}],"name":"getFile","outputs":[{"name":"fileName","type":"string"},{"name":"fileHash","type":"string"}],"payable":false,"stateMutability":"view","type":"function"}]  }');
     var CoursetroContract = web3.eth.contract(contractInfo.abi)
-    var Coursetro = CoursetroContract.at('0x5c21d6021aaee409901461ad2230db16b663182b');
+    var Coursetro = CoursetroContract.at('0x3958c21a7037d4a53c2990834c59ca3dcb00d988');
     var fileName = "vivek.pdf";
+    var identifier = uuid.v4();
+    console.log("uuid is " + identifier);
 
-    Coursetro.setFile(fileName,"test hash2",function(error, result){
+    Coursetro.setFile(identifier,fileName,"test hash2",function(error, result){
       if(!error)
       {
-        console.log("Transaction submitted");
+        console.log("Transaction submitted" + result);
       }
       else{
         console.log("Error " + error);
@@ -38,16 +41,11 @@ module.exports = {
   },
 
   listenEvent: function (req, res) {
-    /* Fallback to local node or remote node
+    /* Fallback to local node or remote node2
        by default local HTTP-RPC server exposes port 8545.
        you can use Infura Node Urls also
        'https://ropsten.infura.io/<API KEy>'*/
 
-    web3js = new Web3(new Web3.providers.HttpProvider('wss://127.0.0.1:8545/ws'));
-    web3.eth.defaultAccount = web3.eth.accounts[0];
-    var contractInfo = JSON.parse('{"contract_name": "FileDetail","abi":[{"constant":false,"inputs":[{"name":"fName","type":"string"},{"name":"fileHash","type":"string"}],"name":"setFile","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"index","type":"uint256"}],"name":"getFile","outputs":[{"name":"fileName","type":"string"},{"name":"fileHash","type":"string"}],"payable":false,"stateMutability":"view","type":"function"}]  }');
-    var CoursetroContract = web3.eth.contract(contractInfo.abi)
-    var Coursetro = CoursetroContract.at('0x5c21d6021aaee409901461ad2230db16b663182b');
 
   },
 };
--- config/bootstrap.js
+++ config/bootstrap.js
@@ -8,10 +8,27 @@
  * For more information on bootstrapping your app, check out:
  * http://sailsjs.org/#!/documentation/reference/sails.config/sails.config.bootstrap.html
  */
-
+var Web3 = require('web3');
+var web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:8545'));
 module.exports.bootstrap = function(cb) {
 
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
+
+//  web3js = new Web3(new Web3.providers.HttpProvider('wss://127.0.0.1:8545/ws'));
+  web3.eth.defaultAccount = web3.eth.accounts[0];
+  var contractInfo = JSON.parse('{"contract_name": "FileDetail","abi":[{"constant":false,"inputs":[{"name":"uuid","type":"string"},{"name":"fName","type":"string"},{"name":"fileHash","type":"string"}],"name":"setFile","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"anonymous":false,"inputs":[{"indexed":false,"name":"fileName","type":"string"},{"indexed":false,"name":"fileHash","type":"string"}],"name":"Uploaded","type":"event"},{"constant":true,"inputs":[{"name":"uuid","type":"string"}],"name":"getFile","outputs":[{"name":"fileName","type":"string"},{"name":"fileHash","type":"string"}],"payable":false,"stateMutability":"view","type":"function"}]  }');
+  var CoursetroContract = web3.eth.contract(contractInfo.abi)
+  var Coursetro = CoursetroContract.at('0x3958c21a7037d4a53c2990834c59ca3dcb00d988');
+
+  var event = Coursetro.Uploaded();
+  event.watch((err, res) => {
+    console.log(res); // event response
+    console.log("event response.."); // event response
+    // Do something on event;
+  });
+
+
+
   cb();
 };
